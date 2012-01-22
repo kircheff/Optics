b876d473481ad94051f731e3af224cce68595e30
Kostadin Petrichkov <petri4kov@gmail.com>
2012-01-22 17:57:46 +0200

    Database search is working!

--- Source/frmDatabase.Designer.vb
+++ Source/frmDatabase.Designer.vb
@@ -23,39 +23,71 @@ Partial Class frmDatabase
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.lstWords = New System.Windows.Forms.ListBox()
-        Me.DGV = New System.Windows.Forms.DataGridView()
-        CType(Me.DGV, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.lblWord = New System.Windows.Forms.Label()
+        Me.lblMeaning = New System.Windows.Forms.Label()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.lstWordsResults = New System.Windows.Forms.ListBox()
         Me.SuspendLayout()
         '
         'lstWords
         '
         Me.lstWords.FormattingEnabled = True
-        Me.lstWords.Location = New System.Drawing.Point(12, 48)
+        Me.lstWords.Location = New System.Drawing.Point(12, 58)
         Me.lstWords.Name = "lstWords"
         Me.lstWords.Size = New System.Drawing.Size(116, 225)
         Me.lstWords.TabIndex = 0
         '
-        'DGV
+        'lblWord
         '
-        Me.DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DGV.Location = New System.Drawing.Point(181, 89)
-        Me.DGV.Name = "DGV"
-        Me.DGV.Size = New System.Drawing.Size(240, 150)
-        Me.DGV.TabIndex = 1
+        Me.lblWord.AutoSize = True
+        Me.lblWord.Location = New System.Drawing.Point(178, 48)
+        Me.lblWord.Name = "lblWord"
+        Me.lblWord.Size = New System.Drawing.Size(0, 13)
+        Me.lblWord.TabIndex = 1
+        '
+        'lblMeaning
+        '
+        Me.lblMeaning.AutoSize = True
+        Me.lblMeaning.Location = New System.Drawing.Point(285, 201)
+        Me.lblMeaning.Name = "lblMeaning"
+        Me.lblMeaning.Size = New System.Drawing.Size(39, 13)
+        Me.lblMeaning.TabIndex = 2
+        Me.lblMeaning.Text = "Label2"
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Location = New System.Drawing.Point(13, 32)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(115, 20)
+        Me.txtSearch.TabIndex = 3
+        '
+        'lstWordsResults
+        '
+        Me.lstWordsResults.FormattingEnabled = True
+        Me.lstWordsResults.Location = New System.Drawing.Point(125, 58)
+        Me.lstWordsResults.Name = "lstWordsResults"
+        Me.lstWordsResults.Size = New System.Drawing.Size(116, 225)
+        Me.lstWordsResults.TabIndex = 4
         '
         'frmDatabase
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(480, 381)
-        Me.Controls.Add(Me.DGV)
+        Me.Controls.Add(Me.lstWordsResults)
+        Me.Controls.Add(Me.txtSearch)
+        Me.Controls.Add(Me.lblMeaning)
+        Me.Controls.Add(Me.lblWord)
         Me.Controls.Add(Me.lstWords)
         Me.Name = "frmDatabase"
         Me.Text = "Database"
-        CType(Me.DGV, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents lstWords As System.Windows.Forms.ListBox
-    Friend WithEvents DGV As System.Windows.Forms.DataGridView
+    Friend WithEvents lblWord As System.Windows.Forms.Label
+    Friend WithEvents lblMeaning As System.Windows.Forms.Label
+    Friend WithEvents txtSearch As System.Windows.Forms.TextBox
+    Friend WithEvents lstWordsResults As System.Windows.Forms.ListBox
 End Class
--- Source/frmDatabase.vb
+++ Source/frmDatabase.vb
@@ -1,20 +1,35 @@
 ﻿Public Class frmDatabase
-    
-    Private Sub frmDatabase_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        Dim con As New OleDb.OleDbConnection
+    Dim con As New OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\Base.accdb")
+    Dim dt As New DataTable("rechnik")
+    Dim rs As New OleDb.OleDbDataAdapter("SELECT * FROM rechnik", con)
 
-        con.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\Base.accdb"
+    Private Sub frmDatabase_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         con.Open()
-        Dim dt As New DataTable("rechnik")
-        Dim rs As OleDb.OleDbDataAdapter
-        rs = New OleDb.OleDbDataAdapter("SELECT * FROM rechnik", con)
+
+
+
         rs.Fill(dt)
         For indx = 0 To dt.Rows.Count - 1
-
-
-
             lstWords.Items.Add(dt.Rows(indx).Item("duma"))
-           
         Next indx
+        con.Close()
+    End Sub
+
+    Private Sub lstWords_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lstWords.SelectedIndexChanged
+        lblWord.Text = dt.Rows(lstWords.SelectedIndex).Item("duma").ToString
+        lblMeaning.Text = dt.Rows(lstWords.SelectedIndex).Item("znachenie").ToString
+    End Sub
+
+    Private Sub txtSearch_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtSearch.TextChanged
+        lstWordsResults.Items.Clear()
+        Dim count As Integer = (lstWords.Items.Count - 1)
+        Dim words As String
+        For a = 0 To count
+            words = lstWords.Items.Item(a)
+            If InStr(words.ToLower, txtSearch.Text.ToLower) = 1 Then
+                lstWordsResults.Items.Add(words)
+            End If
+
+        Next
     End Sub
 End Class
