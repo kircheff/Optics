725028f9aa28fe7bd297b1a86242688c0785803e
Kostadin Petrichkov <petri4kov@gmail.com>
2012-01-22 16:06:09 +0200

    Test commit

--- Source/frmGeneral.vb
+++ Source/frmGeneral.vb
@@ -2,5 +2,6 @@
 
     Private Sub btnCalc_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCalc.Click
         frmCalculator.Show()
+        'Other code goes here
     End Sub
 End Class
