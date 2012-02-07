90ae104c464c40135b6735ceb3f718cb37b5c830
Viktor Kirchev <kircheff@gmail.com>
2012-02-07 20:53:20 +0200

    Simple changes in Crossword

--- Source/Form1.vb
+++ Source/Form1.vb
@@ -8,57 +8,67 @@
     Private Sub frmCrosswordSplash_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         rs.Fill(ds, "Rechnik")
         For i = 0 To ds.Tables("rechnik").Rows.Count - 1
-            lstWords.Items.Add(ds.Tables("rechnik").Rows(i).Item("duma"))
+            If InStr(ds.Tables("rechnik").Rows(i).Item("duma"), " ") = False Then
+                lstWords.Items.Add(ds.Tables("rechnik").Rows(i).Item("duma"))
+            End If
         Next i
 
     End Sub
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        v_duma = lstWords.Items(lstWords.SelectedIndex)
-        lst_words_vis.Items.Clear()
-        lst_pos_vis.Items.Clear()
-        Dim masiv As String
-        masiv = v_duma.ToCharArray
-        Dim pos_start(masiv.Length - 1) As Integer
-        Dim izbrani_dumi(masiv.Length - 1) As String
-        For i = 0 To lstWords.Items.Count - 1
-            For j = 0 To masiv.Count - 1
-                If InStr(lstWords.Items(i), masiv(j)) Then
+        If lstWords.SelectedIndex = -1 Then
+
+            MsgBox("Моля, изберете елемент")
+        Else
+            v_duma = lstWords.Items(lstWords.SelectedIndex)
+
+            lst_words_vis.Items.Clear()
+            lst_pos_vis.Items.Clear()
+            lst_izbrani_vis.Items.Clear()
+
+            Dim masiv As String
+            masiv = v_duma.ToCharArray
+            Dim pos_start(masiv.Length - 1) As Integer
+            Dim izbrani_dumi(masiv.Length - 1) As String
+            For i = 0 To lstWords.Items.Count - 1
+                For j = 0 To masiv.Count - 1
+                    If InStr(lstWords.Items(i), masiv(j)) Then
                         lst_words_vis.Items.Add(lstWords.Items(i))
-                End If
-            Next j
-        Next i
+                    End If
+                Next j
+            Next i
 
-        RemoveDuplicateItem(lst_words_vis)
-        Dim rand As Integer
-        Dim found As Boolean
-        For i = 0 To masiv.Count - 1
-            found = False
-            'rand = RandomNumber(lst_words_vis.Items.Count - 1)
-            Do While found = False
-                rand = RandomNumber(lst_words_vis.Items.Count - 1)
-                If InStr(lst_words_vis.Items(rand), masiv(i)) Then
-                    pos_start(i) = InStr(lst_words_vis.Items(rand), masiv(i))
-
-                    izbrani_dumi(i) = lst_words_vis.Items(rand).ToString
-                    lst_words_vis.Items.RemoveAt(rand)
-                    found = True
+            RemoveDuplicateItem(lst_words_vis)
+            Dim rand As Integer
+            Dim found As Boolean
+            For i = 0 To masiv.Count - 1
+                found = False
+                'rand = RandomNumber(lst_words_vis.Items.Count - 1)
+                Do While found = False
                     rand = RandomNumber(lst_words_vis.Items.Count - 1)
-                Else
-                    found = False
-                End If
+                    If InStr(lst_words_vis.Items(rand), masiv(i)) Then
+                        pos_start(i) = InStr(lst_words_vis.Items(rand), masiv(i))
 
-            Loop
-        Next
+                        izbrani_dumi(i) = lst_words_vis.Items(rand).ToString
+                        lst_words_vis.Items.RemoveAt(rand)
+                        found = True
+                        rand = RandomNumber(lst_words_vis.Items.Count - 1)
+                    Else
+                        found = False
+                    End If
+
+                Loop
+            Next
 
 
-        For i = 0 To pos_start.Count - 1
-            lst_pos_vis.Items.Add(pos_start(i))
-        Next i
+            For i = 0 To pos_start.Count - 1
+                lst_pos_vis.Items.Add(pos_start(i))
+            Next i
 
-        For i = 0 To izbrani_dumi.Count - 1
-            lst_izbrani_vis.Items.Add(izbrani_dumi(i))
-        Next i
+            For i = 0 To izbrani_dumi.Count - 1
+                lst_izbrani_vis.Items.Add(izbrani_dumi(i))
+            Next i
+        End If
     End Sub
 
 
