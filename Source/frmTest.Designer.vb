389b0e8523558b4ebc68c87610330abe8bfcbb88
Kostadin Petrichkov <petri4kov@gmail.com>
2012-02-19 18:06:10 +0200

    Test progress: SUPERB!

--- Source/frmTest.Designer.vb
+++ Source/frmTest.Designer.vb
@@ -22,26 +22,113 @@ Partial Class frmTest
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.components = New System.ComponentModel.Container()
+        Me.lblQuestion = New System.Windows.Forms.Label()
+        Me.opt_answ1 = New System.Windows.Forms.RadioButton()
+        Me.opt_answ2 = New System.Windows.Forms.RadioButton()
+        Me.opt_answ3 = New System.Windows.Forms.RadioButton()
+        Me.tmr_time = New System.Windows.Forms.Timer(Me.components)
+        Me.lbl_seconds = New System.Windows.Forms.Label()
+        Me.cmd_next = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
-        'ProgressBar1
+        'lblQuestion
         '
-        Me.ProgressBar1.Location = New System.Drawing.Point(135, 114)
-        Me.ProgressBar1.Name = "ProgressBar1"
-        Me.ProgressBar1.Size = New System.Drawing.Size(235, 46)
-        Me.ProgressBar1.TabIndex = 0
+        Me.lblQuestion.AutoSize = True
+        Me.lblQuestion.Location = New System.Drawing.Point(164, 78)
+        Me.lblQuestion.Name = "lblQuestion"
+        Me.lblQuestion.Size = New System.Drawing.Size(45, 13)
+        Me.lblQuestion.TabIndex = 0
+        Me.lblQuestion.Text = "Въпрос"
+        '
+        'opt_answ1
+        '
+        Me.opt_answ1.AutoSize = True
+        Me.opt_answ1.Location = New System.Drawing.Point(164, 133)
+        Me.opt_answ1.Name = "opt_answ1"
+        Me.opt_answ1.Size = New System.Drawing.Size(76, 17)
+        Me.opt_answ1.TabIndex = 1
+        Me.opt_answ1.TabStop = True
+        Me.opt_answ1.Text = "Отговор 1"
+        Me.opt_answ1.UseVisualStyleBackColor = True
+        '
+        'opt_answ2
+        '
+        Me.opt_answ2.AutoSize = True
+        Me.opt_answ2.Location = New System.Drawing.Point(164, 156)
+        Me.opt_answ2.Name = "opt_answ2"
+        Me.opt_answ2.Size = New System.Drawing.Size(76, 17)
+        Me.opt_answ2.TabIndex = 2
+        Me.opt_answ2.TabStop = True
+        Me.opt_answ2.Text = "Отговор 1"
+        Me.opt_answ2.UseVisualStyleBackColor = True
+        '
+        'opt_answ3
+        '
+        Me.opt_answ3.AutoSize = True
+        Me.opt_answ3.Location = New System.Drawing.Point(164, 179)
+        Me.opt_answ3.Name = "opt_answ3"
+        Me.opt_answ3.Size = New System.Drawing.Size(76, 17)
+        Me.opt_answ3.TabIndex = 3
+        Me.opt_answ3.TabStop = True
+        Me.opt_answ3.Text = "Отговор 1"
+        Me.opt_answ3.UseVisualStyleBackColor = True
+        '
+        'tmr_time
+        '
+        Me.tmr_time.Enabled = True
+        Me.tmr_time.Interval = 1000
+        '
+        'lbl_seconds
+        '
+        Me.lbl_seconds.AutoSize = True
+        Me.lbl_seconds.Location = New System.Drawing.Point(528, 9)
+        Me.lbl_seconds.Name = "lbl_seconds"
+        Me.lbl_seconds.Size = New System.Drawing.Size(45, 13)
+        Me.lbl_seconds.TabIndex = 4
+        Me.lbl_seconds.Text = "Въпрос"
+        '
+        'cmd_next
+        '
+        Me.cmd_next.Location = New System.Drawing.Point(417, 312)
+        Me.cmd_next.Name = "cmd_next"
+        Me.cmd_next.Size = New System.Drawing.Size(75, 23)
+        Me.cmd_next.TabIndex = 5
+        Me.cmd_next.Text = "Следващ >"
+        Me.cmd_next.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(13, 13)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 6
         '
         'frmTest
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(483, 359)
-        Me.Controls.Add(Me.ProgressBar1)
+        Me.ClientSize = New System.Drawing.Size(585, 390)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.cmd_next)
+        Me.Controls.Add(Me.lbl_seconds)
+        Me.Controls.Add(Me.opt_answ3)
+        Me.Controls.Add(Me.opt_answ2)
+        Me.Controls.Add(Me.opt_answ1)
+        Me.Controls.Add(Me.lblQuestion)
         Me.Name = "frmTest"
-        Me.Text = "Form1"
+        Me.Text = "Тест"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
-    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents lblQuestion As System.Windows.Forms.Label
+    Friend WithEvents opt_answ1 As System.Windows.Forms.RadioButton
+    Friend WithEvents opt_answ2 As System.Windows.Forms.RadioButton
+    Friend WithEvents opt_answ3 As System.Windows.Forms.RadioButton
+    Friend WithEvents tmr_time As System.Windows.Forms.Timer
+    Friend WithEvents lbl_seconds As System.Windows.Forms.Label
+    Friend WithEvents cmd_next As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
 End Class
--- Source/frmTest.vb
+++ Source/frmTest.vb
@@ -1,6 +1,103 @@
 ﻿Public Class frmTest
+    Dim con As New OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\Base.accdb")
+    Dim ds As New DataSet
+    Dim rs As New OleDb.OleDbDataAdapter("SELECT * FROM test", con)
 
+    Dim seconds As Byte 'Секунди
+    Dim question As String 'Текущ въпрос
+    Dim questionID As Integer 'Номер на текущия въпрос
+    Dim answers(2) As String 'Възможни отговори на текущия въпрос
+    Dim correct_answer As Byte 'Верен отговор на текущия въпрос
+    Dim points As Byte 'Текущи точки на потребителя
+    Dim answer_points As Byte 'Колко точки е текущят въпрос
+    Dim user_answer As Byte 'Избран отговор от потребителя
+    Dim questons_answered As Byte 'На колко въпроса е отговорил потребителя досега
     Private Sub frmTest_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        con.Open()
 
+        rs.Fill(ds, "Test")
+
+        NextQuestion()
+
+
+    End Sub
+    Private Sub tmr_time_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmr_time.Tick
+        seconds = seconds + 1
+        lbl_seconds.Text = 60 - seconds & " / 60"
+
+        If seconds >= 60 Then NextQuestion()
+    End Sub
+
+    Private Sub NextQuestion()
+        If questons_answered = 10 Then
+            'frmEdikakvo si .show
+            End
+        End If
+
+        Dim rand As Integer
+
+        seconds = 0
+        rand = RandomNumber(ds.Tables("test").Rows.Count, 0)
+
+        If rand = questionID Then
+            rand = RandomNumber(ds.Tables("test").Rows.Count, 0)
+        End If
+
+        questionID = rand
+        question = ds.Tables("test").Rows(rand).Item("question")
+
+
+        answers(0) = ds.Tables("test").Rows(rand).Item("ans1")
+        answers(1) = ds.Tables("test").Rows(rand).Item("ans2")
+        answers(2) = ds.Tables("test").Rows(rand).Item("ans3")
+
+        correct_answer = ds.Tables("test").Rows(rand).Item("correct")
+        answer_points = ds.Tables("test").Rows(rand).Item("points")
+
+        If opt_answ1.Checked = True Then user_answer = 1
+        If opt_answ2.Checked = True Then user_answer = 2
+        If opt_answ3.Checked = True Then user_answer = 3
+
+        If user_answer = correct_answer Then
+
+            points = points + answer_points
+        End If
+
+        opt_answ1.Text = answers(0).ToString
+        opt_answ2.Text = answers(1).ToString
+        opt_answ3.Text = answers(2).ToString
+
+        lblQuestion.Text = question
+
+
+
+    End Sub
+
+    Public Function RandomNumber(ByVal MaxNumber As Integer, Optional ByVal MinNumber As Integer = 0) As Integer
+
+        'initialize random number generator
+        Dim r As New Random(System.DateTime.Now.Millisecond)
+
+        'if passed incorrect arguments, swap them
+        'can also throw exception or return 0
+
+        If MinNumber > MaxNumber Then
+            Dim t As Integer = MinNumber
+            MinNumber = MaxNumber
+            MaxNumber = t
+        End If
+
+        Return r.Next(MinNumber, MaxNumber)
+
+    End Function
+
+    Private Sub cmd_next_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmd_next.Click
+        NextQuestion()
+        TextBox1.Text = points
+
+    End Sub
+
+    Private Sub Clear_array(ByVal array As Array)
+        array.Clear(array, array.GetLowerBound(0), array.Length)
     End Sub
 End Class
