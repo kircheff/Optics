8a6f2fddd8f0fc95d0ca75f1f95bb56b44b32f49
Kostadin Petrichkov <petri4kov@gmail.com>
2012-02-28 03:55:46 +0200

    Design: finished without gallery

--- Source/frmGeneral.Designer.vb
+++ Source/frmGeneral.Designer.vb
@@ -22,6 +22,8 @@ Partial Class frmGeneral
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmGeneral))
         Me.btnCalc = New System.Windows.Forms.Button()
         Me.btnDiction = New System.Windows.Forms.Button()
         Me.btnAdmin = New System.Windows.Forms.Button()
@@ -48,12 +50,35 @@ Partial Class frmGeneral
         Me.ЗаПрограматаToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.ЗаАвторитеToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.ИзползванаЛитератураToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.pic_diction = New System.Windows.Forms.PictureBox()
+        Me.pic_crossword = New System.Windows.Forms.PictureBox()
+        Me.pic_info = New System.Windows.Forms.PictureBox()
+        Me.pic_search = New System.Windows.Forms.PictureBox()
+        Me.pic_test = New System.Windows.Forms.PictureBox()
+        Me.pic_admin = New System.Windows.Forms.PictureBox()
+        Me.pic_gallery = New System.Windows.Forms.PictureBox()
+        Me.pic_calc = New System.Windows.Forms.PictureBox()
+        Me.pic_arrow = New System.Windows.Forms.PictureBox()
+        Me.pic_game = New System.Windows.Forms.PictureBox()
+        Me.tmrMove = New System.Windows.Forms.Timer(Me.components)
         Me.Menu.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic_diction, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic_crossword, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic_info, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic_search, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic_test, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic_admin, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic_gallery, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic_calc, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic_arrow, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic_game, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'btnCalc
         '
-        Me.btnCalc.Location = New System.Drawing.Point(128, 75)
+        Me.btnCalc.Location = New System.Drawing.Point(591, 27)
         Me.btnCalc.Name = "btnCalc"
         Me.btnCalc.Size = New System.Drawing.Size(126, 29)
         Me.btnCalc.TabIndex = 0
@@ -62,7 +87,7 @@ Partial Class frmGeneral
         '
         'btnDiction
         '
-        Me.btnDiction.Location = New System.Drawing.Point(260, 75)
+        Me.btnDiction.Location = New System.Drawing.Point(591, 132)
         Me.btnDiction.Name = "btnDiction"
         Me.btnDiction.Size = New System.Drawing.Size(126, 29)
         Me.btnDiction.TabIndex = 1
@@ -71,25 +96,25 @@ Partial Class frmGeneral
         '
         'btnAdmin
         '
-        Me.btnAdmin.Location = New System.Drawing.Point(392, 145)
+        Me.btnAdmin.Location = New System.Drawing.Point(591, 307)
         Me.btnAdmin.Name = "btnAdmin"
-        Me.btnAdmin.Size = New System.Drawing.Size(104, 29)
+        Me.btnAdmin.Size = New System.Drawing.Size(126, 29)
         Me.btnAdmin.TabIndex = 2
         Me.btnAdmin.Text = "Админ"
         Me.btnAdmin.UseVisualStyleBackColor = True
         '
         'btnCrossword
         '
-        Me.btnCrossword.Location = New System.Drawing.Point(392, 75)
+        Me.btnCrossword.Location = New System.Drawing.Point(591, 237)
         Me.btnCrossword.Name = "btnCrossword"
-        Me.btnCrossword.Size = New System.Drawing.Size(104, 29)
+        Me.btnCrossword.Size = New System.Drawing.Size(126, 29)
         Me.btnCrossword.TabIndex = 3
         Me.btnCrossword.Text = "Кръстословица"
         Me.btnCrossword.UseVisualStyleBackColor = True
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(128, 110)
+        Me.Button2.Location = New System.Drawing.Point(591, 62)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(126, 29)
         Me.Button2.TabIndex = 5
@@ -98,7 +123,7 @@ Partial Class frmGeneral
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(260, 110)
+        Me.Button1.Location = New System.Drawing.Point(591, 167)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(126, 29)
         Me.Button1.TabIndex = 6
@@ -107,16 +132,16 @@ Partial Class frmGeneral
         '
         'cmdGallery
         '
-        Me.cmdGallery.Location = New System.Drawing.Point(392, 110)
+        Me.cmdGallery.Location = New System.Drawing.Point(591, 272)
         Me.cmdGallery.Name = "cmdGallery"
-        Me.cmdGallery.Size = New System.Drawing.Size(104, 29)
+        Me.cmdGallery.Size = New System.Drawing.Size(126, 29)
         Me.cmdGallery.TabIndex = 7
         Me.cmdGallery.Text = "Галерия"
         Me.cmdGallery.UseVisualStyleBackColor = True
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(128, 145)
+        Me.Button3.Location = New System.Drawing.Point(591, 97)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(126, 29)
         Me.Button3.TabIndex = 8
@@ -125,7 +150,7 @@ Partial Class frmGeneral
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(260, 145)
+        Me.Button4.Location = New System.Drawing.Point(591, 202)
         Me.Button4.Name = "Button4"
         Me.Button4.Size = New System.Drawing.Size(126, 29)
         Me.Button4.TabIndex = 9
@@ -137,7 +162,7 @@ Partial Class frmGeneral
         Me.Menu.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ТеорияToolStripMenuItem, Me.ПровериЗнаниятаСиToolStripMenuItem, Me.ЗабавноToolStripMenuItem, Me.КалкулаторToolStripMenuItem, Me.АдминПанелToolStripMenuItem, Me.ТърсенеToolStripMenuItem, Me.ПомощToolStripMenuItem})
         Me.Menu.Location = New System.Drawing.Point(0, 0)
         Me.Menu.Name = "Menu"
-        Me.Menu.Size = New System.Drawing.Size(714, 24)
+        Me.Menu.Size = New System.Drawing.Size(642, 24)
         Me.Menu.TabIndex = 10
         Me.Menu.Text = "MenuStrip1"
         '
@@ -151,13 +176,13 @@ Partial Class frmGeneral
         'УрочниСтатииToolStripMenuItem
         '
         Me.УрочниСтатииToolStripMenuItem.Name = "УрочниСтатииToolStripMenuItem"
-        Me.УрочниСтатииToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.УрочниСтатииToolStripMenuItem.Size = New System.Drawing.Size(149, 22)
         Me.УрочниСтатииToolStripMenuItem.Text = "Урочни статии"
         '
         'РечникToolStripMenuItem
         '
         Me.РечникToolStripMenuItem.Name = "РечникToolStripMenuItem"
-        Me.РечникToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.РечникToolStripMenuItem.Size = New System.Drawing.Size(149, 22)
         Me.РечникToolStripMenuItem.Text = "Речник"
         '
         'ПровериЗнаниятаСиToolStripMenuItem
@@ -189,13 +214,13 @@ Partial Class frmGeneral
         'ИграToolStripMenuItem
         '
         Me.ИграToolStripMenuItem.Name = "ИграToolStripMenuItem"
-        Me.ИграToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ИграToolStripMenuItem.Size = New System.Drawing.Size(116, 22)
         Me.ИграToolStripMenuItem.Text = "Игра"
         '
         'ГалерияToolStripMenuItem
         '
         Me.ГалерияToolStripMenuItem.Name = "ГалерияToolStripMenuItem"
-        Me.ГалерияToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ГалерияToolStripMenuItem.Size = New System.Drawing.Size(116, 22)
         Me.ГалерияToolStripMenuItem.Text = "Галерия"
         '
         'КалкулаторToolStripMenuItem
@@ -241,11 +266,154 @@ Partial Class frmGeneral
         Me.ИзползванаЛитератураToolStripMenuItem.Size = New System.Drawing.Size(196, 22)
         Me.ИзползванаЛитератураToolStripMenuItem.Text = "Използвана литература"
         '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(484, -102)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(113, 569)
+        Me.PictureBox1.TabIndex = 11
+        Me.PictureBox1.TabStop = False
+        '
+        'pic_diction
+        '
+        Me.pic_diction.BackColor = System.Drawing.Color.Transparent
+        Me.pic_diction.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.pic_diction.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.pic_diction.Image = CType(resources.GetObject("pic_diction.Image"), System.Drawing.Image)
+        Me.pic_diction.Location = New System.Drawing.Point(40, 27)
+        Me.pic_diction.Name = "pic_diction"
+        Me.pic_diction.Size = New System.Drawing.Size(422, 42)
+        Me.pic_diction.TabIndex = 12
+        Me.pic_diction.TabStop = False
+        '
+        'pic_crossword
+        '
+        Me.pic_crossword.BackColor = System.Drawing.Color.Transparent
+        Me.pic_crossword.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.pic_crossword.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.pic_crossword.Image = CType(resources.GetObject("pic_crossword.Image"), System.Drawing.Image)
+        Me.pic_crossword.Location = New System.Drawing.Point(40, 75)
+        Me.pic_crossword.Name = "pic_crossword"
+        Me.pic_crossword.Size = New System.Drawing.Size(422, 42)
+        Me.pic_crossword.TabIndex = 13
+        Me.pic_crossword.TabStop = False
+        '
+        'pic_info
+        '
+        Me.pic_info.BackColor = System.Drawing.Color.Transparent
+        Me.pic_info.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.pic_info.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.pic_info.Image = CType(resources.GetObject("pic_info.Image"), System.Drawing.Image)
+        Me.pic_info.Location = New System.Drawing.Point(40, 123)
+        Me.pic_info.Name = "pic_info"
+        Me.pic_info.Size = New System.Drawing.Size(422, 42)
+        Me.pic_info.TabIndex = 14
+        Me.pic_info.TabStop = False
+        '
+        'pic_search
+        '
+        Me.pic_search.BackColor = System.Drawing.Color.Transparent
+        Me.pic_search.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.pic_search.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.pic_search.Image = CType(resources.GetObject("pic_search.Image"), System.Drawing.Image)
+        Me.pic_search.Location = New System.Drawing.Point(40, 171)
+        Me.pic_search.Name = "pic_search"
+        Me.pic_search.Size = New System.Drawing.Size(422, 42)
+        Me.pic_search.TabIndex = 15
+        Me.pic_search.TabStop = False
+        '
+        'pic_test
+        '
+        Me.pic_test.BackColor = System.Drawing.Color.Transparent
+        Me.pic_test.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.pic_test.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.pic_test.Image = CType(resources.GetObject("pic_test.Image"), System.Drawing.Image)
+        Me.pic_test.Location = New System.Drawing.Point(40, 219)
+        Me.pic_test.Name = "pic_test"
+        Me.pic_test.Size = New System.Drawing.Size(422, 42)
+        Me.pic_test.TabIndex = 16
+        Me.pic_test.TabStop = False
+        '
+        'pic_admin
+        '
+        Me.pic_admin.BackColor = System.Drawing.Color.Transparent
+        Me.pic_admin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.pic_admin.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.pic_admin.Image = CType(resources.GetObject("pic_admin.Image"), System.Drawing.Image)
+        Me.pic_admin.Location = New System.Drawing.Point(40, 267)
+        Me.pic_admin.Name = "pic_admin"
+        Me.pic_admin.Size = New System.Drawing.Size(422, 42)
+        Me.pic_admin.TabIndex = 17
+        Me.pic_admin.TabStop = False
+        '
+        'pic_gallery
+        '
+        Me.pic_gallery.BackColor = System.Drawing.Color.Transparent
+        Me.pic_gallery.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.pic_gallery.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.pic_gallery.Image = CType(resources.GetObject("pic_gallery.Image"), System.Drawing.Image)
+        Me.pic_gallery.Location = New System.Drawing.Point(40, 315)
+        Me.pic_gallery.Name = "pic_gallery"
+        Me.pic_gallery.Size = New System.Drawing.Size(422, 42)
+        Me.pic_gallery.TabIndex = 18
+        Me.pic_gallery.TabStop = False
+        '
+        'pic_calc
+        '
+        Me.pic_calc.BackColor = System.Drawing.Color.Transparent
+        Me.pic_calc.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.pic_calc.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.pic_calc.Image = CType(resources.GetObject("pic_calc.Image"), System.Drawing.Image)
+        Me.pic_calc.Location = New System.Drawing.Point(40, 363)
+        Me.pic_calc.Name = "pic_calc"
+        Me.pic_calc.Size = New System.Drawing.Size(422, 42)
+        Me.pic_calc.TabIndex = 19
+        Me.pic_calc.TabStop = False
+        '
+        'pic_arrow
+        '
+        Me.pic_arrow.BackColor = System.Drawing.Color.Transparent
+        Me.pic_arrow.Image = CType(resources.GetObject("pic_arrow.Image"), System.Drawing.Image)
+        Me.pic_arrow.Location = New System.Drawing.Point(-580, 450)
+        Me.pic_arrow.Name = "pic_arrow"
+        Me.pic_arrow.Size = New System.Drawing.Size(562, 97)
+        Me.pic_arrow.TabIndex = 20
+        Me.pic_arrow.TabStop = False
+        '
+        'pic_game
+        '
+        Me.pic_game.BackColor = System.Drawing.Color.Transparent
+        Me.pic_game.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.pic_game.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.pic_game.Image = CType(resources.GetObject("pic_game.Image"), System.Drawing.Image)
+        Me.pic_game.Location = New System.Drawing.Point(40, 411)
+        Me.pic_game.Name = "pic_game"
+        Me.pic_game.Size = New System.Drawing.Size(422, 42)
+        Me.pic_game.TabIndex = 21
+        Me.pic_game.TabStop = False
+        '
+        'tmrMove
+        '
+        Me.tmrMove.Interval = 50
+        '
         'frmGeneral
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(714, 244)
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(642, 573)
+        Me.Controls.Add(Me.pic_game)
+        Me.Controls.Add(Me.pic_arrow)
+        Me.Controls.Add(Me.pic_calc)
+        Me.Controls.Add(Me.pic_gallery)
+        Me.Controls.Add(Me.pic_admin)
+        Me.Controls.Add(Me.pic_test)
+        Me.Controls.Add(Me.pic_search)
+        Me.Controls.Add(Me.pic_info)
+        Me.Controls.Add(Me.pic_crossword)
+        Me.Controls.Add(Me.pic_diction)
         Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.cmdGallery)
@@ -256,11 +424,23 @@ Partial Class frmGeneral
         Me.Controls.Add(Me.btnDiction)
         Me.Controls.Add(Me.btnCalc)
         Me.Controls.Add(Me.Menu)
+        Me.Controls.Add(Me.PictureBox1)
         Me.MainMenuStrip = Me.Menu
         Me.Name = "frmGeneral"
         Me.Text = "Начална страница"
         Me.Menu.ResumeLayout(False)
         Me.Menu.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic_diction, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic_crossword, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic_info, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic_search, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic_test, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic_admin, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic_gallery, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic_calc, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic_arrow, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic_game, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -291,5 +471,17 @@ Partial Class frmGeneral
     Friend WithEvents ЗабавноToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
     Friend WithEvents ИграToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
     Friend WithEvents ГалерияToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents pic_diction As System.Windows.Forms.PictureBox
+    Friend WithEvents pic_crossword As System.Windows.Forms.PictureBox
+    Friend WithEvents pic_info As System.Windows.Forms.PictureBox
+    Friend WithEvents pic_search As System.Windows.Forms.PictureBox
+    Friend WithEvents pic_test As System.Windows.Forms.PictureBox
+    Friend WithEvents pic_admin As System.Windows.Forms.PictureBox
+    Friend WithEvents pic_gallery As System.Windows.Forms.PictureBox
+    Friend WithEvents pic_calc As System.Windows.Forms.PictureBox
+    Friend WithEvents pic_arrow As System.Windows.Forms.PictureBox
+    Friend WithEvents pic_game As System.Windows.Forms.PictureBox
+    Friend WithEvents tmrMove As System.Windows.Forms.Timer
 
 End Class
--- Source/frmGeneral.vb
+++ Source/frmGeneral.vb
@@ -1,5 +1,7 @@
 ﻿Public Class frmGeneral
-
+    Dim destination As String
+    Dim numOfMoves As Byte = 0
+    Dim visibleImageIndex As Integer = 0
     Private Sub btnCalc_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCalc.Click
         frmCalculator.Show()
 
@@ -101,5 +103,171 @@
     Private Sub ИзползванаЛитератураToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ИзползванаЛитератураToolStripMenuItem.Click
         frmInfoLit.Show()
 
+    End Sub
+
+    Private Sub frmGeneral_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'Разположение на дясна стрелка: -30, 450
+        'Разположение на горна стрелка: 510, -35
+
+    End Sub
+
+    Private Sub tmrMove_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrMove.Tick
+
+        numOfMoves = numOfMoves + 1
+        moveImages(destination)
+        If numOfMoves = 10 Then
+
+            numOfMoves = 0
+            tmrMove.Stop()
+
+        End If
+    End Sub
+
+    Sub moveImages(ByVal destination As String)
+        If destination = "Right" Then
+
+            pic_arrow.Left = pic_arrow.Left - 50
+
+        ElseIf destination = "Left" Then
+
+            pic_arrow.Left = pic_arrow.Left + 50
+
+        End If
+    End Sub
+
+    Private Sub pic_diction_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles pic_diction.Click
+        frmDictionary.Show()
+
+    End Sub
+
+    Private Sub pic_diction_MouseHover(ByVal sender As Object, ByVal e As System.EventArgs) Handles pic_diction.MouseHover
+        pic_arrow.Image = System.Drawing.Bitmap.FromFile(AppDomain.CurrentDomain.BaseDirectory & "Images\General\arrow_dictionary.png")
+        destination = "Left"
+        tmrMove.Start()
+    End Sub
+
+    Private Sub pic_diction_MouseLeave(ByVal sender As Object, ByVal e As System.EventArgs) Handles pic_diction.MouseLeave
+        pic_arrow.Left = -580
+        numOfMoves = 0
+    End Sub
+
+    Private Sub pic_crossword_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles pic_crossword.Click
+        frmCrossword.Show()
+    End Sub
+
+    Private Sub pic_crossword_MouseHover(ByVal sender As Object, ByVal e As System.EventArgs) Handles pic_crossword.MouseHover
+        pic_arrow.Image = System.Drawing.Bitmap.FromFile(AppDomain.CurrentDomain.BaseDirectory & "Images\General\arrow_crossword.png")
+        destination = "Left"
+        tmrMove.Start()
+    End Sub
+
+    Private Sub pic_crossword_MouseLeave(ByVal sender As Object, ByVal e As System.EventArgs) Handles pic_crossword.MouseLeave
+        pic_arrow.Left = -580
+        numOfMoves = 0
+    End Sub
+
+    Private Sub pic_gallery_MouseHover(ByVal sender As Object, ByVal e As System.EventArgs) Handles pic_gallery.MouseHover
+        pic_arrow.Image = System.Drawing.Bitmap.FromFile(AppDomain.CurrentDomain.BaseDirectory & "Images\General\arrow_gallery.png")
+        destination = "Left"
+        tmrMove.Start()
+    End Sub
+
+    Private Sub pic_gallery_MouseLeave(ByVal sender As Object, ByVal e As System.EventArgs) Handles pic_gallery.MouseLeave
+        pic_arrow.Left = -580
+        numOfMoves = 0
+    End Sub
+
+    Private Sub pic_info_MouseHover(ByVal sender As Object, ByVal e As System.EventArgs) Handles pic_info.MouseHover
+        pic_arrow.Image = System.Drawing.Bitmap.FromFile(AppDomain.CurrentDomain.BaseDirectory & "Images\General\arrow_info.png")
+        destination = "Left"
+        tmrMove.Start()
+    End Sub
+
+    Private Sub pic_info_MouseLeave(ByVal sender As Object, ByVal e As System.EventArgs) Handles pic_info.MouseLeave
+        pic_arrow.Left = -580
+        numOfMoves = 0
+    End Sub
+
+    Private Sub pic_search_MouseHover(ByVal sender As Object, ByVal e As System.EventArgs) Handles pic_search.MouseHover
+        pic_arrow.Image = System.Drawing.Bitmap.FromFile(AppDomain.CurrentDomain.BaseDirectory & "Images\General\arrow_search.png")
+        destination = "Left"
+        tmrMove.Start()
+    End Sub
+
+    Private Sub pic_search_MouseLeave(ByVal sender As Object, ByVal e As System.EventArgs) Handles pic_search.MouseLeave
+        pic_arrow.Left = -580
+        numOfMoves = 0
+    End Sub
+
+    Private Sub pic_admin_MouseHover(ByVal sender As Object, ByVal e As System.EventArgs) Handles pic_admin.MouseHover
+        pic_arrow.Image = System.Drawing.Bitmap.FromFile(AppDomain.CurrentDomain.BaseDirectory & "Images\General\arrow_admin.png")
+        destination = "Left"
+        tmrMove.Start()
+    End Sub
+
+    Private Sub pic_admin_MouseLeave(ByVal sender As Object, ByVal e As System.EventArgs) Handles pic_admin.MouseLeave
+        pic_arrow.Left = -580
+        numOfMoves = 0
+    End Sub
+
+    Private Sub pic_calc_MouseHover(ByVal sender As Object, ByVal e As System.EventArgs) Handles pic_calc.MouseHover
+        pic_arrow.Image = System.Drawing.Bitmap.FromFile(AppDomain.CurrentDomain.BaseDirectory & "Images\General\arrow_calc.png")
+        destination = "Left"
+        tmrMove.Start()
+    End Sub
+
+    Private Sub pic_calc_MouseLeave(ByVal sender As Object, ByVal e As System.EventArgs) Handles pic_calc.MouseLeave
+        pic_arrow.Left = -580
+        numOfMoves = 0
+    End Sub
+
+    Private Sub pic_test_MouseHover(ByVal sender As Object, ByVal e As System.EventArgs) Handles pic_test.MouseHover
+        pic_arrow.Image = System.Drawing.Bitmap.FromFile(AppDomain.CurrentDomain.BaseDirectory & "Images\General\arrow_test.png")
+        destination = "Left"
+        tmrMove.Start()
+    End Sub
+
+    Private Sub pic_test_MouseLeave(ByVal sender As Object, ByVal e As System.EventArgs) Handles pic_test.MouseLeave
+        pic_arrow.Left = -580
+        numOfMoves = 0
+    End Sub
+
+    Private Sub pic_game_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles pic_game.Click
+        frmGame.Show()
+    End Sub
+
+    Private Sub pic_game_MouseHover(ByVal sender As Object, ByVal e As System.EventArgs) Handles pic_game.MouseHover
+        pic_arrow.Image = System.Drawing.Bitmap.FromFile(AppDomain.CurrentDomain.BaseDirectory & "Images\General\arrow_game.png")
+        destination = "Left"
+        tmrMove.Start()
+    End Sub
+
+    Private Sub pic_game_MouseLeave(ByVal sender As Object, ByVal e As System.EventArgs) Handles pic_game.MouseLeave
+        pic_arrow.Left = -580
+        numOfMoves = 0
+    End Sub
+
+    Private Sub pic_info_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles pic_info.Click
+        frmInformation.Show()
+    End Sub
+
+    Private Sub pic_search_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles pic_search.Click
+        frmSearch.Show()
+    End Sub
+
+    Private Sub pic_test_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles pic_test.Click
+        frmTest.Show()
+    End Sub
+
+    Private Sub pic_admin_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles pic_admin.Click
+        frmAdminSplash.Show()
+    End Sub
+
+    Private Sub pic_gallery_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles pic_gallery.Click
+        frmGallerySplash.Show()
+    End Sub
+
+    Private Sub pic_calc_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles pic_calc.Click
+        frmCalculator.Show()
     End Sub
 End Class
